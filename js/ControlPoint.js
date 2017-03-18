f4091ad8b6cd8a2e735f4e92070e4fe6ae33831a
amandaghassaei <amandaghassaei@gmail.com>
2017-03-18 00:50:24 -0400

    draggable control pts

--- js/ControlPoint.js
+++ js/ControlPoint.js
@@ -4,12 +4,14 @@
 
 
 var cpMat = new THREE.MeshBasicMaterial({color:0xff00ff});
+var cpMatHighlight = new THREE.MeshBasicMaterial({color:0xffaaff});
 var cpGeo = new THREE.CircleGeometry(1);
 
 function ControlPoint(position){
 
     this.mesh = new THREE.Mesh(cpGeo, cpMat);
     this.mesh.position.set(position.x, position.y, position.z);
+    this.mesh._myNode = this;
     this.setScale();
     threeView.scene.add(this.mesh);
 }
@@ -23,7 +25,24 @@ ControlPoint.prototype.setScale = function(){
     this.mesh.scale.set(scale, scale, scale);
 };
 
+ControlPoint.prototype.getMesh = function(){
+    return this.mesh;
+};
+
+ControlPoint.prototype.highlight = function(){
+    this.mesh.material = cpMatHighlight;
+};
+
+ControlPoint.prototype.unhighlight = function(){
+    this.mesh.material = cpMat;
+};
+
+ControlPoint.prototype.moveManually = function(position){
+    this.mesh.position.set(position.x, position.y, 1);
+};
+
 ControlPoint.prototype.destroy = function(){
     threeView.scene.remove(this.mesh);
+    this.mesh._myNode = null;
     this.mesh = null;
 };
--- js/dataView.js
+++ js/dataView.js
@@ -6,7 +6,16 @@ var plane;
 var vertices;
 var boundary;
 var controlPoints;
+var cpMeshes;
 var mesh;
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var raycasterPlane = new THREE.Plane(new THREE.Vector3(0,0,1));
+var isDragging = false;
+var draggingCP = null;
+var mouseDown = false;
+var highlightedObj;
 
 function initDataView(){
     var planeGeo = new THREE.PlaneBufferGeometry(1, 1);
@@ -16,6 +25,21 @@ function initDataView(){
     threeView.scene.add(plane);
 
     threeView.controls.addEventListener('change', updateControlPoints);
+
+     document.addEventListener('mousedown', function(){
+        mouseDown = true;
+    }, false);
+
+    document.addEventListener('mouseup', function(e){
+        isDragging = false;
+        if (draggingCP){
+            draggingCP = null;
+            setHighlightedObj(null);
+        }
+        mouseDown = false;
+    }, false);
+
+    document.addEventListener( 'mousemove', mouseMove, false );
 }
 
 function showData(data) {
@@ -34,12 +58,14 @@ function makeBoundaryGeometry(numPoints){
     if (numPoints <= 0) numPoints = 6;
     vertices = [];
     controlPoints = [];
+    cpMeshes = [];
     var geometry = new THREE.Geometry();
     for (var i=0;i<numPoints;i++){
         var theta = i*Math.PI*2/numPoints;
         var cp = new ControlPoint(new THREE.Vector3(Math.cos(theta), Math.sin(theta), 1));
         vertices.push(cp.getPosition());
         controlPoints.push(cp);
+        cpMeshes.push(cp.getMesh());
     }
     vertices.push(vertices[0]);
     geometry.vertices = vertices;
@@ -47,7 +73,7 @@ function makeBoundaryGeometry(numPoints){
     var material = new THREE.LineBasicMaterial({
         color: 0xff00ff
     });
-    boundary = new THREE.Line( geometry, material );
+    boundary = new THREE.Line(geometry, material);
     threeView.scene.add(boundary);
     threeView.render();
 }
@@ -57,4 +83,70 @@ function updateControlPoints(){
     for (var i=0;i<controlPoints.length;i++){
         controlPoints[i].setScale();
     }
+}
+
+function mouseMove(e){
+
+    if (controlPoints == undefined || controlPoints.length <= 0) return;
+
+    if (mouseDown) {
+        isDragging = true;
+    }
+
+    e.preventDefault();
+    mouse.x = (e.clientX/window.innerWidth)*2-1;
+    mouse.y = - (e.clientY/window.innerHeight)*2+1;
+    raycaster.setFromCamera(mouse, threeView.camera);
+
+    var _highlightedObj = null;
+    if (!isDragging) {
+        var objsToIntersect = cpMeshes;
+        _highlightedObj = checkForIntersections(e, objsToIntersect);
+        setHighlightedObj(_highlightedObj);
+    }  else if (isDragging && highlightedObj){
+        if (!draggingCP) {
+            draggingCP = highlightedObj;
+        }
+        var intersection = getIntersectionWithObjectPlane(highlightedObj.getPosition().clone());
+        highlightedObj.moveManually(intersection);
+        boundary.geometry.verticesNeedUpdate = true;
+        threeView.render();
+    }
+}
+
+function getIntersectionWithObjectPlane(position){
+    // var cameraOrientation = threeView.camera.getWorldDirection();
+    // var dist = position.dot(cameraOrientation);
+    // raycasterPlane.set(cameraOrientation, -dist);
+    var intersection = new THREE.Vector3();
+    raycaster.ray.intersectPlane(raycasterPlane, intersection);
+    return intersection;
+}
+
+function setHighlightedObj(object){
+    if (highlightedObj && (object != highlightedObj)) {
+        highlightedObj.unhighlight();
+        // globals.controls.hideMoreInfo();
+    }
+    highlightedObj = object;
+    if (highlightedObj) highlightedObj.highlight();
+    threeView.render();
+}
+
+function checkForIntersections(e, objects){
+    var _highlightedObj = null;
+    var intersections = raycaster.intersectObjects(objects, true);
+    if (intersections.length > 0) {
+        var objectFound = false;
+        _.each(intersections, function (thing) {
+            if (objectFound) return;
+            if (thing.object && thing.object._myNode){
+                _highlightedObj = thing.object._myNode;
+                if (!_highlightedObj.fixed) return;
+                _highlightedObj.highlight();
+                objectFound = true;
+            }
+        });
+    }
+    return _highlightedObj;
 }
